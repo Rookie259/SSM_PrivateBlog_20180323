04cf10aa275c30bc60e837a7983f950c980be479
沐惜 <13213731716@163.com>
2018-04-03 16:47:23 +0800

    创建项目1.0.0

--- src/main/java/com/muxi/reids/ssm/controller/ImageUploadController.java
+++ src/main/java/com/muxi/reids/ssm/controller/ImageUploadController.java
@@ -9,21 +9,19 @@ import com.aliyun.oss.OSSClient;
 import com.muxi.reids.ssm.tool.aliyun.OSSObjectLiteral;
 import com.muxi.reids.ssm.tool.aliyun.OSSTool;
 import com.muxi.reids.ssm.tool.commonTools.MultipartFileTranFile;
-import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-/*测试*/
+/*测试  github更新*/
 @Controller
 @RequestMapping("writeBlog")
 public class ImageUploadController {
